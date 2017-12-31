57a4dc6f719cbcacbe75ee64464ecc6e4d021461
Sayra Ranjha <S.S.Ranjha@student.tudelft.nl>
2017-12-31 15:29:44 +0100

    Update bash scripts to run measure test

--- examples/nativeMode/prepareQubit/run.sh
+++ examples/nativeMode/prepareQubit/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-cd "$NETSIM/examples/nativeMode/graphState"
-python bobTest.py &
-python charlieTest.py &
-python davidTest.py &
-python aliceTest.py
+cd "$NETSIM/examples/nativeMode/prepareQubit"
+python measureTest.py
