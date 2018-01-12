84aa9d319bd598c84cd332be081bcb0d16d0d4f9
Axel Dahlberg <axel.dahlberg12@gmail.com>
2018-01-12 10:07:09 +0100

    Fixed problem with startClassicalServer when running program multiple times

--- cqc/pythonLib/cqc.py
+++ cqc/pythonLib/cqc.py
@@ -132,6 +132,7 @@ class CQCConnection:
 
 			# Setup server
 			s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 			s.bind((myHost.hostname,myHost.port))
 			s.listen(1)
 			(conn,addr)=s.accept()
