133512bd56a789badc1ea3381293d22a4da0b9be
Axel Dahlberg <axel.dahlberg12@gmail.com>
2018-04-17 12:55:17 +0200

    Added configFiles.py to help setup networks with different nodes (docs)

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, '/Users/adahlberg/anaconda3/lib/python3.6/site-packages/')
+import os
+import sys
+sys.path.insert(0, '/Users/adahlberg/anaconda3/lib/python3.6/site-packages/')
 
 
 # -- General configuration ------------------------------------------------
