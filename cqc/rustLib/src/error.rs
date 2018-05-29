9eaf369e64eeb028daccff0705abbeeeb01df805
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2018-05-29 13:33:38 +0100

    Implement the API

--- cqc/rustLib/src/error.rs
+++ cqc/rustLib/src/error.rs
@@ -2,23 +2,27 @@ use std::io;
 use std::fmt;
 use std::error::Error;
 
+use bincode;
+
 #[derive(Debug)]
 pub enum CqcError {
     General,
-    Io(io::Error),
     NoQubit,
     Unsupported,
     Timeout,
+    Io(io::Error),
+    Serde(Box<bincode::ErrorKind>),
 }
 
 impl Error for CqcError {
     fn description(&self) -> &str {
         match self {
             &CqcError::General => "General error",
-            &CqcError::Io(ref io_err) => io_err.description(),
             &CqcError::NoQubit => "No qubit with the specified ID for this application",
             &CqcError::Unsupported => "Command not supported by implementation",
             &CqcError::Timeout => "Timeout",
+            &CqcError::Io(ref io_err) => io_err.description(),
+            &CqcError::Serde(ref sd_err) => sd_err.description(),
         }
     }
 }
@@ -35,5 +39,11 @@ impl fmt::Display for CqcError {
 impl From<io::Error> for CqcError {
     fn from(error: io::Error) -> Self {
         CqcError::Io(error)
+    }
+}
+
+impl From<Box<bincode::ErrorKind>> for CqcError {
+    fn from(error: Box<bincode::ErrorKind>) -> Self {
+        CqcError::Serde(error)
     }
 }
--- cqc/rustLib/src/lib.rs
+++ cqc/rustLib/src/lib.rs
@@ -1,8 +1,16 @@
+extern crate bincode;
+#[macro_use]
+extern crate serde_derive;
+
 use std::net;
-use std::error::Error;
+
+use bincode::{deserialize_from, serialize_into};
 
 mod error;
 use error::CqcError;
+
+mod cqc_api;
+use cqc_api::*;
 
 pub struct Cqc {
     app_id: u16,
@@ -15,49 +23,226 @@ impl Cqc {
         Ok(Cqc { app_id, stream })
     }
 
-    pub fn simple_cmd(&self, command: u8, qubit_id: u16, notify: u8) {}
+    pub fn simple_cmd(&self, command: u8, qubit_id: u16, notify: bool) -> Result<(), CqcError> {
+        // Prepare CQC message indicating a command.
+        let cqc_header = CqcHeader {
+            version: CQC_VERSION,
+            ctrl_type: CQC_TP_COMMAND,
+            app_id: self.app_id,
+            length: CQC_CMD_HDR_LENGTH,
+        };
+
+        // Send message to the server.
+        serialize_into(&self.stream, &cqc_header)?;
+
+        // Prepare message for the specific command.
+        let cmd_header = CmdHeader {
+            qubit_id,
+            instr: command,
+            options: if notify {
+                CQC_OPT_NOTIFY | CQC_OPT_BLOCK
+            } else {
+                0x00
+            },
+        };
+
+        // Send message to the server.
+        serialize_into(&self.stream, &cmd_header)?;
+
+        Ok(())
+    }
+
     pub fn full_cmd(
         &self,
         command: u8,
         qubit_id: u16,
-        notify: char,
-        action: char,
-        block: char,
+        notify: bool,
+        action: bool,
+        block: bool,
         xtra_id: u16,
         steps: u8,
         r_app_id: u16,
         r_node: u32,
         r_port: u16,
-        cmdLength: u32,
-    ) {
+        cmd_length: u32,
+    ) -> Result<(), CqcError> {
+        // Prepare a CQC message indicating a command
+        let cqc_header = CqcHeader {
+            version: CQC_VERSION,
+            ctrl_type: CQC_TP_COMMAND,
+            app_id: self.app_id,
+            length: CQC_CMD_HDR_LENGTH + CQC_CMD_XTRA_LENGTH,
+        };
+
+        // Send message to the server.
+        serialize_into(&self.stream, &cqc_header)?;
+
+        // Prepare message for the specific command.
+        let mut options: u8 = 0;
+        if notify {
+            options = options | CQC_OPT_NOTIFY;
+        }
+        if action {
+            options = options | CQC_OPT_ACTION;
+        }
+        if block {
+            options = options | CQC_OPT_BLOCK;
+        }
+
+        let cmd_header = CmdHeader {
+            qubit_id,
+            instr: command,
+            options,
+        };
+
+        // Send message to the server.
+        serialize_into(&self.stream, &cmd_header)?;
+
+        // Prepare extra header.
+        let xtra_header = XtraCmdHeader {
+            xtra_qubit_id: xtra_id,
+            steps,
+            r_app_id,
+            r_node,
+            r_port,
+            cmd_length,
+        };
+
+        // Send message to the server.
+        serialize_into(&self.stream, &xtra_header)?;
+
+        Ok(())
     }
 
-    pub fn hello(&self) {}
-    pub fn send(&self, qubit_id: u16, r_app_id: u16, r_node: u32, r_port: u16) {}
-    pub fn recv(&self) {}
-    pub fn epr(&self, r_app_id: u16, r_node: u32, r_port: u16) {}
-    pub fn measure(&self, qubit_id: u16) {}
-    pub fn wait_until_done(&self, reps: usize) {}
-    pub fn wait_until_newok(&self) {}
-    pub fn two_qubit(&self, command: u8, qubit_1_id: u16, qubit_2_id: u16) {}
-    pub fn tomography_dir<F>(&self, func: F, iter: u32, dir: u8)
-    where
-        F: Fn(&Cqc) -> u16,
-    {
+    pub fn hello(&self) -> Result<(), CqcError> {
+        // Prepare a CQC message indicating a command.
+        let cqc_header = CqcHeader {
+            version: CQC_VERSION,
+            ctrl_type: CQC_TP_HELLO,
+            app_id: self.app_id,
+            length: CQC_CMD_HDR_LENGTH,
+        };
+
+        // Send message to the server.
+        serialize_into(&self.stream, &cqc_header)?;
+
+        Ok(())
     }
-    pub fn test_qubit<F>(
+
+    pub fn send(
         &self,
-        func: F,
-        iter: u32,
-        epsilon: f64,
-        exp_x: f64,
-        exp_y: f64,
-        exp_z: f64,
-    ) where
-        F: Fn(&Cqc) -> u16,
-    {
+        qubit_id: u16,
+        r_app_id: u16,
+        r_node: u32,
+        r_port: u16,
+    ) -> Result<(), CqcError> {
+        self.full_cmd(
+            CQC_CMD_SEND,
+            qubit_id,
+            true,
+            false,
+            false,
+            0,
+            0,
+            r_app_id,
+            r_node,
+            r_port,
+            0,
+        )
     }
 
+    pub fn recv(&self) -> Result<u16, CqcError> {
+        // Send out a request to receive a qubit.
+        self.simple_cmd(CQC_CMD_RECV, 0, false)?;
+
+        // Now read CQC header from server response.
+        let reply: CqcHeader = deserialize_from(&self.stream)?;
+
+        if reply.ctrl_type != CQC_TP_RECV {
+            return Err(CqcError::General);
+        }
+
+        // Read qubit id from the notify header.
+        let note: NotifyHeader = deserialize_from(&self.stream)?;
+
+        Ok(note.qubit_id)
+    }
+
+    pub fn epr(&self, r_app_id: u16, r_node: u32, r_port: u16) -> Result<(), CqcError> {
+        self.full_cmd(
+            CQC_CMD_RECV,
+            0,
+            false,
+            false,
+            true,
+            0,
+            0,
+            r_app_id,
+            r_node,
+            r_port,
+            0,
+        )
+    }
+
+    pub fn measure(&self, qubit_id: u16) -> Result<u8, CqcError> {
+        // Send a CQC message to request measurement.
+        self.simple_cmd(CQC_CMD_MEASURE, qubit_id, false)?;
+
+        // Read the response.
+        let reply: CqcHeader = deserialize_from(&self.stream)?;
+
+        if reply.ctrl_type != CQC_TP_MEASOUT {
+            return Err(CqcError::General);
+        }
+
+        // Read measurement outcome.
+        let note: NotifyHeader = deserialize_from(&self.stream)?;
+
+        Ok(note.outcome)
+    }
+
+    pub fn wait_until_done(&self, reps: usize) -> Result<(), CqcError> {
+        // Read the CQC header from the server response.
+        for _ in 0..reps {
+            let reply: CqcHeader = deserialize_from(&self.stream)?;
+
+            if reply.ctrl_type >= CQC_ERR_GENERAL || reply.ctrl_type != CQC_TP_DONE {
+                return Err(CqcError::General);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn wait_until_newok(&self) -> Result<u16, CqcError> {
+        // Read the CQC header from the server response.
+        let reply: CqcHeader = deserialize_from(&self.stream)?;
+
+        if reply.ctrl_type >= CQC_ERR_GENERAL || reply.ctrl_type != CQC_TP_NEW_OK {
+            return Err(CqcError::General);
+        }
+
+        // Read the qubit id.
+        let note: NotifyHeader = deserialize_from(&self.stream)?;
+
+        Ok(note.qubit_id)
+    }
+
+    pub fn two_qubit(&self, command: u8, qubit_1_id: u16, qubit_2_id: u16) -> Result<(), CqcError> {
+        self.full_cmd(
+            command,
+            qubit_1_id,
+            false,
+            false,
+            true,
+            qubit_2_id,
+            0,
+            0,
+            0,
+            0,
+            0,
+        )
+    }
 }
 
 impl Drop for Cqc {
