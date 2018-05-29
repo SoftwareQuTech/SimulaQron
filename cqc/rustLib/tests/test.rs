127db8d77c786cddf8d3f080d51929cde94d4319
Marco <cylammarco@gmail.com>
2018-05-29 23:21:08 +0000

    Remove redundant inline comments.

--- cqc/rustLib/tests/test.rs
+++ cqc/rustLib/tests/test.rs
@@ -17,11 +17,9 @@ fn test_init() {
     cqc.simple_cmd(CQC_CMD_NEW, 0, false).unwrap();
     let qubit = cqc.wait_until_newok().unwrap();
 
-    //Cqc.simple_cmd(cqc_cmd_h, qubit, 1);
     cqc.simple_cmd(CQC_CMD_H, qubit, true).unwrap();
     cqc.wait_until_done(1).unwrap();
 
-    // hard-coded to get an outcome for now
     let outcome: u8 = cqc.measure(qubit).unwrap();
     println!("Outcome: {}", outcome);
 }
