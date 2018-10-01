a9edabdf58a5034c0e58ce3237bc6b91d1a3c81a
Axel Dahlberg <axel.dahlberg12@gmail.com>
2018-10-01 17:36:06 +0200

    Added Makefile such that one can easily run tests or check dependencies, finalized the unittests for the restricted topology

--- tests/auto/network_topology/testAll.sh
+++ tests/auto/network_topology/testAll.sh
@@ -6,18 +6,19 @@ if [ -f "${NETSIM}/config/settings.ini" ]; then
 fi
 
 echo "Creating a new temporary settings file with restricted topology"
-SETTINGS_FILE="${NETSIM}/config/settings.ini"
-touch "${SETTINGS_FILE}"
-echo "[BACKEND]" >> "${SETTINGS_FILE}"
-echo "maxqubits = 20" >> "${SETTINGS_FILE}"
-echo "maxregisters = 1000" >> "${SETTINGS_FILE}"
-echo "waittime = 0.5" >> "${SETTINGS_FILE}"
-echo "loglevel = warning" >> "${SETTINGS_FILE}"
-echo "backendhandler = simulaqron" >> "${SETTINGS_FILE}"
-echo "topology_file = tests/auto/network_topology/resources/topology.json" >> "${SETTINGS_FILE}"
-echo "" >> "${SETTINGS_FILE}"
-echo "[FRONTEND]" >> "${SETTINGS_FILE}"
-echo "loglevel = warning" >> "${SETTINGS_FILE}"
+cp "resources/settings.ini" "${NETSIM}/config/settings.ini"
+#SETTINGS_FILE="${NETSIM}/config/settings.ini"
+#touch "${SETTINGS_FILE}"
+#echo "[BACKEND]" >> "${SETTINGS_FILE}"
+#echo "maxqubits = 20" >> "${SETTINGS_FILE}"
+#echo "maxregisters = 1000" >> "${SETTINGS_FILE}"
+#echo "waittime = 0.5" >> "${SETTINGS_FILE}"
+#echo "loglevel = warning" >> "${SETTINGS_FILE}"
+#echo "backendhandler = simulaqron" >> "${SETTINGS_FILE}"
+#echo "topology_file = tests/auto/network_topology/resources/topology.json" >> "${SETTINGS_FILE}"
+#echo "" >> "${SETTINGS_FILE}"
+#echo "[FRONTEND]" >> "${SETTINGS_FILE}"
+#echo "loglevel = warning" >> "${SETTINGS_FILE}"
 
 echo "Starting SimulaQron sever (restricted topology)"
 sh "${NETSIM}/run/startAll.sh" Alice Bob Charlie &
@@ -42,6 +43,6 @@ fi
 
 # Start servers again for future tests
 echo "Starting SimulaQron sever (default settings)"
-sh "${NETSIM}/run/startAll.sh" Alice Bob Charlie &
+sh "${NETSIM}/run/startAll.sh" Alice Bob Charlie David Eve &
 sleep 1s
 echo "Started SimulaQron sever (default settings)"
