368f88c5a3c0aa3124052df4aa2d776f09b4cd9a
Axel Dahlberg <axel.dahlberg12@gmail.com>
2018-10-06 20:37:54 +0200

    Conflict

--- tests/manual/test_context.py
+++ tests/manual/test_context.py
@@ -1,5 +1,6 @@
 from SimulaQron.cqc.pythonLib.cqc import CQCConnection, qubit
 
+<<<<<<< HEAD
 for _ in range(100):
 
 	with CQCConnection("Alice") as cqc:
@@ -9,3 +10,16 @@ for _ in range(100):
 	# q.H()
 	#
 	# q.measure()
+=======
+cqc = CQCConnection("Alice")
+for _ in range(100):
+
+
+	q = qubit(cqc)
+
+	q.H()
+
+	# m = q.measure()
+	#
+	# print(m)
+>>>>>>> b2af0281e7d3d7f228cdf6e4fec229ea5768b762
