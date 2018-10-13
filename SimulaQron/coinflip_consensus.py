6aaf09bb2bd6ff1e3256842dc8c344c9838296ef
Alexander Mikhalevich <alex.mikhalevich@gmail.com>
2018-10-13 19:30:44 +0000

    Add bias

--- SimulaQron/coinflip_consensus.py
+++ SimulaQron/coinflip_consensus.py
@@ -1,13 +1,19 @@
 from SimulaQron.cqc.pythonLib.cqc import *
+import math
 
 class CoinflipConsensus:
     def __init__(self, queue):
         self.queue = queue
 
-    def _atomic_flip(self, candidate1, candidate2):
+    def _atomic_flip(self, candidate1, candidate2, coeff):
         with CQCConnection(candidate1) as Alice:
             qcandidate1 = Alice.createEPR(candidate2)  # |00> + |11>.
             qcandidate1.X()
+            
+            # Bias
+            angle = 2 * math.acos(coeff)
+            qcandidate.rot_Y(angle * 256 / (2 * math.pi))
+
             # Measure the qubits.
             measured_value = qcandidate1.measure()
 
@@ -20,9 +26,12 @@ class CoinflipConsensus:
                 return candidate2
 
     def leader(self):
+        assert queue.size >= 2
+
         winner = self.queue.pop()  # Returns "top" and removes it.
 
-        for elem in self.queue:
-            winner = self._atomic_flip(winner, elem)
+        for i in range(2, self.queue.size):
+            coeff = 1 / math.sqrt(i)
+            winner = self._atomic_flip(winner, elem, coeff)
 
         return winner
