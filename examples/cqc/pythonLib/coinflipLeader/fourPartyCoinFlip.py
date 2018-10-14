3aee14e743e10a15944264d9d4982e08c2cf1d0f
andersrehult <andersrehult@gmail.com>
2018-10-14 14:16:01 +0200

    Add docstrings

--- examples/cqc/pythonLib/coinflipLeader/fourPartyCoinFlip.py
+++ examples/cqc/pythonLib/coinflipLeader/fourPartyCoinFlip.py
@@ -2,16 +2,17 @@ import time
 
 from SimulaQron.cqc.pythonLib.protocols.coinflip_leader import CoinflipConsensus
 
-#####################################################################################################
-#
-# main
-#
 def main():
+        """
+        Creates array of four people and elects a leader among them using 
+        the CoinFlipConsensus protocol.
+        """
         arr = ["Alice", "Bob","Charlie","David"]
         leaderChooser = CoinflipConsensus(arr)
         return leaderChooser.leader()
         
-##################################################################################################
+
+# Runs 20 rounds of leader election and prints the results.
 d = dict()
 d['Alice'] = 0
 d['Bob'] = 0
@@ -22,4 +23,3 @@ for i in range(0,20):
                 print(i)
         d[main()] += 1
 print(d)
-
