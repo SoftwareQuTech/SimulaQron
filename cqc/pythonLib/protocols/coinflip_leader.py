d8d0ce2fe89df1426b02c910c72dc6e5bd598a3a
Alexander Mikhalevich <alex.mikhalevich@gmail.com>
2018-10-14 15:35:29 +0000

    Add more docs

--- cqc/pythonLib/protocols/coinflip_leader.py
+++ cqc/pythonLib/protocols/coinflip_leader.py
@@ -4,11 +4,19 @@ import math
 class CoinflipConsensus:
     def __init__(self, queue):
         """
-        Inits the algo with the list of candidates' ids.
+        Inits the algo with the list of candidates ids.
+        :param queue: the list of candidates ids.
         """
         self.queue = queue
 
     def _atomic_flip(self, candidate1, candidate2, coeff):
+        """
+        Performs the basic biased coinflip between two parties.
+        :param candidate1: the first party id.
+        :param candidate2: the second party id.
+        :param coeff: bias.
+        :return: the winner id.
+        """
         with CQCConnection(candidate1) as Alice:
             qA = qubit(Alice)
             qB = qubit(Alice)
@@ -22,7 +30,6 @@ class CoinflipConsensus:
 
             # Send qubit qB to Bob.
             Alice.sendQubit(qB,candidate2)
-
             
             # Measure the qubits.
             measured_value = qA.measure()
@@ -40,13 +47,15 @@ class CoinflipConsensus:
     def leader(self):
         """
         Executes the coinflip leader election algo.
-        :return the selected leader id.
+        :return: the selected leader id.
         """
         assert len(self.queue) >= 2
 
         winner = self.queue[0]
         
-        for i in range(2,len(self.queue) + 1):
+        for i in range(2, len(self.queue) + 1):
+            # This is calculated to bias the every next coinflip to
+            # equalize candidates' chances to win.
             coeff = math.sqrt(1/i)
             winner = self._atomic_flip(winner, self.queue[i - 1], coeff)
 
