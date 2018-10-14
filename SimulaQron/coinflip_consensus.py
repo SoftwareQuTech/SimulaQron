dce1b0a31afef2b0a69384cd3ed7a425fa66deab
andersrehult <andersrehult@gmail.com>
2018-10-14 11:46:16 +0200

    Remove debug prints, set amount of runs to 1000

--- SimulaQron/coinflip_consensus.py
+++ SimulaQron/coinflip_consensus.py
@@ -11,22 +11,23 @@ class CoinflipConsensus:
             qB = qubit(Alice)
             
             # Bias
-            print("coeff = "+str(coeff*coeff))
             angle = 2 * math.acos(coeff)
             step = int(angle * 256 / (2 * math.pi))
-            print("step = "+str(step))
             qA.rot_Y(step)
             qA.cnot(qB)
             qB.X()
 
             # Send qubit qB to Bob.
             Alice.sendQubit(qB,candidate2)
+
             
             # Measure the qubits.
             measured_value = qA.measure()
 
-            # TODO: Is it right?
-            # del qcandidate1
+            with CQCConnection(candidate2) as Bob:
+                qB = Bob.recvQubit()
+                bob_value = qB.measure()
+                assert measured_value + bob_value == 1
 
             if measured_value == 1:  # `candidate1` is a winner.
                 return candidate1
@@ -40,7 +41,6 @@ class CoinflipConsensus:
         
         for i in range(2,len(self.queue) + 1):
             coeff = math.sqrt(1/i)
-            print(i)
             winner = self._atomic_flip(winner, self.queue[i - 1], coeff)
 
         return winner
