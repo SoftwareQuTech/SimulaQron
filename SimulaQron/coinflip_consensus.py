153a1f253d1d39e44dc169f2d72ecc18b54e5b61
andersrehult <andersrehult@gmail.com>
2018-10-14 10:25:07 +0200

    Modify bias for coin flips

--- SimulaQron/coinflip_consensus.py
+++ SimulaQron/coinflip_consensus.py
@@ -7,16 +7,23 @@ class CoinflipConsensus:
 
     def _atomic_flip(self, candidate1, candidate2, coeff):
         with CQCConnection(candidate1) as Alice:
-            qcandidate1 = Alice.createEPR(candidate2)  # |00> + |11>.
-            qcandidate1.X()
+            qA = qubit(Alice)
+            qB = qubit(Alice)
             
             # Bias
+            print("coeff = "+str(coeff*coeff))
             angle = 2 * math.acos(coeff)
             step = int(angle * 256 / (2 * math.pi))
-            qcandidate1.rot_Y(step)
+            print("step = "+str(step))
+            qA.rot_Y(step)
+            qA.cnot(qB)
+            qB.X()
 
+            # Send qubit qB to Bob.
+            Alice.sendQubit(qB,candidate2)
+            
             # Measure the qubits.
-            measured_value = qcandidate1.measure()
+            measured_value = qA.measure()
 
             # TODO: Is it right?
             # del qcandidate1
@@ -30,9 +37,10 @@ class CoinflipConsensus:
         assert len(self.queue) >= 2
 
         winner = self.queue[0]
-
-        for i in range(2, len(self.queue) + 1):
-            coeff = 1 / math.sqrt(i)
+        
+        for i in range(2,len(self.queue) + 1):
+            coeff = math.sqrt(1/i)
+            print(i)
             winner = self._atomic_flip(winner, self.queue[i - 1], coeff)
 
         return winner
