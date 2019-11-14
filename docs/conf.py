f5d976bafb9468645b81d243d9fd5e4581c878c4
Axel Dahlberg <axel.dahlberg12@gmail.com>
2019-11-14 16:02:27 +0100

    docs(docs/conf.py): Added extension for github pages so that .nojekyll is added

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,13 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.mathjax"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+]
 
 mathjax_path = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
