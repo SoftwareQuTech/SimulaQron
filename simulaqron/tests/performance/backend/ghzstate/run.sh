5faac84a89a627deb117b88faef6a3b1192e6e5a
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-01-27 18:30:42 +0100

    Better run-script for the performance test

--- simulaqron/tests/performance/backend/ghzstate/run.sh
+++ simulaqron/tests/performance/backend/ghzstate/run.sh
@@ -1,7 +1,7 @@
 simulaqron stop
-simulaqron reset
+simulaqron reset -f
 simulaqron set backend stabilizer
 simulaqron set max-qubits 1000
 simulaqron set max-registers 1000
-simulaqron start
+simulaqron start -f
 python3 test_ghz_state.py "$@"
