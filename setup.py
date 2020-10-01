3a768181399b9ddcfc7c235e4e5f645832dcef96
Axel Dahlberg <axel.dahlberg12@gmail.com>
2020-10-01 13:45:26 +0200

    Exclude tests examples and docs from package

--- setup.py
+++ setup.py
@@ -31,9 +31,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SoftwareQuTech/SimulaQron",
     include_package_data=True,
-    packages=setuptools.find_packages(include=("simulaqron*",)),
+    packages=setuptools.find_packages(exclude=('tests', 'docs', 'examples')),
     package_data={
-        'simulaqron': ['config/.keep', '.simulaqron_pids/.keep', 'tests/unittests/slow/merges/configs/*.cfg']
+        'simulaqron': ['config/.keep', '.simulaqron_pids/.keep']
     },
     install_requires=install_requires,
     python_requires='>=3.5',
