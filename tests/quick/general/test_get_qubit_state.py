7f128cdcd163ed6fc20b806a181de3856eee523a
Diego Rivera <dieriverav@gmail.com>
2025-09-08 17:14:10 +0200

    Add test cases for peeking basic and unflushed qubits

--- tests/quick/general/test_get_qubit_state.py
+++ tests/quick/general/test_get_qubit_state.py
@@ -1,3 +1,4 @@
+import numpy as np
 from netqasm.runtime.settings import set_simulator
 
 set_simulator("simulaqron")
@@ -11,17 +12,32 @@ from simulaqron.run.run import run_applications  # noqa: E402
 
 class TestGetQubit:
     @staticmethod
+    def peek_unflushed_qubit():
+        with NetQASMConnection("Alice") as alice:
+            q_a = Qubit(alice)
+            state_a = get_qubit_state(q_a)
+            return state_a
+
+    @staticmethod
+    def peek_init_qubit():
+        with NetQASMConnection("Alice") as alice:
+            q_a = Qubit(alice)
+            alice.flush()
+            state_a = get_qubit_state(q_a)
+            return state_a
+
+    @staticmethod
     def peek_local_qubit():
         with NetQASMConnection("Alice") as alice:
-            qA = Qubit(alice)
-            qB = Qubit(alice)
-            qA.H()
-            qB.X()
+            q_a = Qubit(alice)
+            q_b = Qubit(alice)
+            q_a.H()
+            q_b.X()
 
             alice.flush()
-            stateA = get_qubit_state(qA)
-            stateB = get_qubit_state(qB)
-            return stateB
+            state_a = get_qubit_state(q_a)
+            state_b = get_qubit_state(q_b)
+            return state_a, state_b
 
     @staticmethod
     def alice_teleport():
@@ -48,6 +64,26 @@ class TestGetQubit:
             entangled_qubit = epr_socket.recv_keep()[0]
             meas = entangled_qubit.measure()
         return meas
+
+    def test_peek_unflushed_qubit(self):
+        # TODO - This method should raise an Exception; not freeze the simulation
+        #  Maybe we need to re-throw exceptions from lower levers?
+        apps = default_app_instance(
+            [
+                ("Alice", TestGetQubit.peek_init_qubit)
+            ]
+        )
+        _ = run_applications(apps, use_app_config=False, enable_logging=False)
+        assert False
+
+    def test_get_basic_state_local(self):
+        apps = default_app_instance(
+            [
+                ("Alice", TestGetQubit.peek_init_qubit)
+            ]
+        )
+        raw_results = run_applications(apps, use_app_config=False, enable_logging=False)
+        assert np.array_equal(raw_results[0]["app_Alice"], np.array([1.0+0j, 0+0j]))
 
     def test_get_qubit_state_local(self):
         apps = default_app_instance(
