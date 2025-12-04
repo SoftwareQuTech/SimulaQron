1016eeaf9532159e1792a05676b12cf3679ef32b
Diego Rivera <dieriverav@gmail.com>
2025-12-04 12:51:25 +0100

    Adapt test for olf json format to use a temporal file that will be reformatted

--- tests/quick/settings/test_networks_settings.py
+++ tests/quick/settings/test_networks_settings.py
@@ -262,24 +262,30 @@ class TestNetworksSettings:
     def test_load_old_json_format(self):
         this_file_folder = Path(__file__).parent
         old_json_config_path = this_file_folder / "resources" / "old_format.json"
-        network_config.read_from_file(old_json_config_path)
 
-        assert len(network_config.networks) == 1
-        assert network_config.networks[0].name == "default"
-        assert len(network_config.nodes) == 2
+        with NamedTemporaryFile(mode="wt", delete_on_close=False) as temp_file:
+            shutil.copy(old_json_config_path, temp_file.name)
+            network_config.read_from_file(temp_file.name)
 
-        assert network_config.nodes[0].name == "Alice"
-        assert network_config.nodes[0].app_hostname == "localhost"
-        assert network_config.nodes[0].app_port == 8821
-        assert network_config.nodes[0].qnodeos_hostname == "localhost"
-        assert network_config.nodes[0].qnodeos_port == 8822
-        assert network_config.nodes[0].vnode_hostname == "localhost"
-        assert network_config.nodes[0].qnodeos_port == 8823
+            raw_converted = json.load(temp_file)
+            assert isinstance(raw_converted, dict)
 
-        assert network_config.nodes[1].name == "Bob"
-        assert network_config.nodes[1].app_hostname == "localhost"
-        assert network_config.nodes[1].app_port == 8831
-        assert network_config.nodes[1].qnodeos_hostname == "localhost"
-        assert network_config.nodes[1].qnodeos_port == 8832
-        assert network_config.nodes[1].vnode_hostname == "localhost"
-        assert network_config.nodes[1].qnodeos_port == 8833
+            assert len(network_config.networks) == 1
+            assert network_config.networks[0].name == "default"
+            assert len(network_config.nodes) == 2
+
+            assert network_config.nodes[0].name == "Alice"
+            assert network_config.nodes[0].app_hostname == "localhost"
+            assert network_config.nodes[0].app_port == 8821
+            assert network_config.nodes[0].qnodeos_hostname == "localhost"
+            assert network_config.nodes[0].qnodeos_port == 8822
+            assert network_config.nodes[0].vnode_hostname == "localhost"
+            assert network_config.nodes[0].qnodeos_port == 8823
+
+            assert network_config.nodes[1].name == "Bob"
+            assert network_config.nodes[1].app_hostname == "localhost"
+            assert network_config.nodes[1].app_port == 8831
+            assert network_config.nodes[1].qnodeos_hostname == "localhost"
+            assert network_config.nodes[1].qnodeos_port == 8832
+            assert network_config.nodes[1].vnode_hostname == "localhost"
+            assert network_config.nodes[1].qnodeos_port == 8833
