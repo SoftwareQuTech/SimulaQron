994842113bbcb8ac1a77cb32e1b97e303a86daad
Diego Rivera <dieriverav@gmail.com>
2026-05-08 12:40:29 +0200

    Use twisted's "pollreactor" on macOS environments

--- simulaqron/reactor.py
+++ simulaqron/reactor.py
@@ -1,6 +1,24 @@
-from twisted.internet import asyncioreactor
+import sys
 
-asyncioreactor.install()
+if sys.platform == "darwin":
+    from twisted.internet import pollreactor
+
+    pollreactor.install()
+
+if sys.platform == "linux":
+    # TODO - Find out which is the right twisted reactor for Linux
+    #  Currently "asyncioreactor" works, and allow interoperability with asyncio primitives.
+    #  However, this reactor is slow, and asyncio support is not needed on the background
+    #  processes that run the simulaqron backend.
+    from twisted.internet import asyncioreactor
+
+    asyncioreactor.install()
+
+if sys.platform == "win32":
+    # TODO - Find out which is the right twisted reactor for Win
+    #  Hint: Claude says that "selectreactor"  and "IOCPReactor" are supported on Windows
+    from twisted.internet import selectreactor
+    selectreactor.install()
 
 from twisted.internet import reactor  # noqa: E402
 
@@ -15,7 +33,7 @@ rather than the one available in the ``twisted.internet`` package::
 
 from simulaqron.reactor import reactor
 ...
-reactor.start()
+reactor.run()
 ...
 reactor.stop()
 """
